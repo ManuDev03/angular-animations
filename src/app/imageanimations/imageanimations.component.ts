dd6d8753e655c6618ac7c2d68f210f7dfe2fe7b2
ManuDev03 <manuprajan03@gmail.com>
2021-07-29 21:22:54 +0530

    expertzT16-image rotation animation

--- src/app/imageanimations/imageanimations.component.ts
+++ src/app/imageanimations/imageanimations.component.ts
@@ -1,11 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import {trigger,state,transition,style,animate,animation} from '@angular/animations'
 @Component({
   selector: 'app-imageanimations',
   templateUrl: './imageanimations.component.html',
-  styleUrls: ['./imageanimations.component.css']
+  styleUrls: ['./imageanimations.component.css'],
+  animations:[trigger("imageTransitions",[
+    state("idle",style({transform:'rotate(0)'})),
+    state("rotated",style({transform:'rotate(-180deg)'})),
+    transition("idle=>rotated",animate('400ms ease-in')),
+    transition("rotated=>idle",animate('400ms ease-out'))
+
+  ])]
 })
 export class ImageanimationsComponent implements OnInit {
+state =true
+toggle(){
+  this.state=this.state?false:true
+}
 
   constructor() { }
 
