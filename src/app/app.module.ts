930a1664432024322b12ccc30d325c74a3bb4d98
ManuDev03 <manuprajan03@gmail.com>
2021-07-30 13:22:07 +0530

    creating routing object adding components to array

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule ,routingComponents} from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyanimationsComponent } from './myanimations/myanimations.component';
 import { ImageanimationsComponent } from './imageanimations/imageanimations.component';
-import { AboutComponent } from './about/about.component';
-import { ProductComponent } from './product/product.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MyanimationsComponent,
     ImageanimationsComponent,
-    AboutComponent,
-    ProductComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
